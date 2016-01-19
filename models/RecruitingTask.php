175d2dea3cc44d08d6bfa0fb914e849e32a8d51d
flashadvocate <flashadvocate@gmail.com>
2016-01-18 21:10:12 -0500

    add sort order for recruiting tasks

--- models/RecruitingTask.php
+++ models/RecruitingTask.php
@@ -2,15 +2,19 @@
 
 class RecruitingTask extends Application
 {
+    public static $id_field = 'id';
+    public static $table = 'recruiting_tasks';
     public $id;
     public $content;
     public $game_id;
-
-    public static $id_field = 'id';
-    public static $table = 'recruiting_tasks';
+    public $sort_order;
 
     public static function findAll($game_id)
     {
-        return self::find(array('game_id @' => array(0, $game_id)));
+        return arrayToObject(Flight::aod()->using('RecruitingTask')
+            ->where(array('game_id @' => array(0, $game_id)))
+            ->sortAsc('game_id')
+            ->sortAsc('sort_order')
+            ->find());
     }
 }
